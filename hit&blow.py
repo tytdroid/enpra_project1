a03a3ce6c0eb336a20790d577e620e28ea0dcdc6
tytdroid <k86843347@gmail.com>
2025-06-29 16:58:11 +0900

    Add hit/blow judgment logic

--- hit&blow.py
+++ hit&blow.py
@@ -8,4 +8,8 @@ while len(answer) < 3:
 
 your_answer = input("Enter your 3-digit guess：")
 
+hit = sum([int(your_answer[i]) == answer[i] for i in range(3)])
+blow = sum([int(g) in answer for g in your_answer]) - hit
+print(f"{hit} Hit, {blow} Blow")
+
 print(answer,your_answer)      
