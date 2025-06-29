34e65ecb12cff81cbb033b1d9e5b1d11dc58d5c7
tytdroid <k86843347@gmail.com>
2025-06-29 16:53:13 +0900

    Add user input for 3-digit guess

--- hit&blow.py
+++ hit&blow.py
@@ -1,9 +1,11 @@
 import random
 
-kotae = []
-while len(kotae) != 3:
-	x = random.randint(1,9)
-	if x not in kotae:
-		kotae.append(x)
+answer = []
+while len(answer) < 3:
+    x = random.randint(1, 9)
+    if x not in answer:
+        answer.append(x)
 
-print(kotae)
+your_answer = input("Enter your 3-digit guess：")
+
+print(answer,your_answer)      
