c92afc90dace82dd79b7bbfd1bb7cfc315d7defc
tytdroid <k86843347@gmail.com>
2025-06-29 17:10:58 +0900

    add answer limit

--- hit&blow.py
+++ hit&blow.py
@@ -1,15 +1,23 @@
 import random
 
-answer = []
+limit=2#回数制限
+
+answer = []#答えの設定
 while len(answer) < 3:
     x = random.randint(1, 9)
     if x not in answer:
         answer.append(x)
 
-your_answer = input("Enter your 3-digit guess：")
+for i in range(limit):
+    your_answer = input("Enter your 3-digit guess：")#回答の入力
 
-hit = sum([int(your_answer[i]) == answer[i] for i in range(3)])
-blow = sum([int(g) in answer for g in your_answer]) - hit
-print(f"{hit} Hit, {blow} Blow")
+    hit = sum([int(your_answer[i]) == answer[i] for i in range(3)])#結果＆ヒント
+    blow = sum([int(g) in answer for g in your_answer]) - hit
+    print(f"{hit} Hit, {blow} Blow")
+    print(answer,your_answer)
 
-print(answer,your_answer)      
+    if your_answer == ''.join(map(str, answer)):
+        print("Correct!")
+        break
+else:
+    print(f"You failed! The answer was {''.join(map(str, answer))}")  
